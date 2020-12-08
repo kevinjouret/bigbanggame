f3895659e64fa8776a6093a6e99d891c3282651f
Kevin J <75573377+kevinjouret@users.noreply.github.com>
2020-12-08 19:10:37 +0100

    Remplacement LightBox > Modal

--- script.js
+++ script.js
@@ -109,31 +109,16 @@ const compare = () =>{
     CscoreSpan.textContent = cScore
     
     if(pScore == 10){
-        alert("Vous avez gagné !")
+        alert("BAZINGAAAA !!!\nRound Terminé : Tu as gagné !");
+        pScore = 0;
+        cScore = 0;
     }
     else if(cScore == 10){
-        alert("Vous avez perdu !")
+        alert("LOSER !!! ^^\nRound Terminé : Tu as perdu !")
+        pScore = 0;
+        cScore = 0;
     }
-}
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
 }
 
 
